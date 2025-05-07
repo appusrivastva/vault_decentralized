1d9312d5fe7c6cadb911b4afcee0691f7189197c
intel <apoorvadunitech@gmail.com>
2025-05-07 11:46:27 +0530

    add files

--- vault/src/utils/connectWallet.js
+++ vault/src/utils/connectWallet.js
@@ -1,27 +1,36 @@
 
 import ethers from "ethers"
 import {contractAbi} from "../constant/contractAbi.json"
+import toast from "react-hot-toast"
 export const connectWallet=async ()=>{
-    if(!window.ethereum){
-        throw new Error("please install metamask")
+    try{
+        if(!window.ethereum){
+            throw new Error("please install metamask")
+        }
+    
+        const accounts=await window.ethereum.request({
+            method:"eth_requestAccounts"
+        })
+        console.log(accounts[0])
+    
+        const selectedAccounts=accounts[0]
+    
+    
+        const provider=ethers.BrowserProvider(window.ethereum)
+    
+        const signer=await provider.getSigner()
+    
+        const contractAddress=import.meta.env.VITE_CONTRACT_ADDRESS
+        console.log(contractAddress)
+    
+        const contractInstance=new ethers.Contract(contractAddress,contractAbi,signer)
+    
+        return (contractInstance,selectedAccounts)
+    }
+    catch(error){
+        toast.error("metamask connection failed")
+        console.log(error)
     }
 
-    const accounts=await window.ethereum.request({
-        method:"eth_requestAccounts"
-    })
-    console.log(accounts[0])
-
-    const selectedAccounts=accounts[0]
-
-
-    const provider=ethers.BrowserProvider(window.ethereum)
-
-    const signer=provider.getSigner()
-
-    const contractAddress=""
-
-    const contractInstance=new ethers.Contract(contractAddress,contractAbi,signer)
-
-    return (contractInstance,selectedAccounts)
 
 }
